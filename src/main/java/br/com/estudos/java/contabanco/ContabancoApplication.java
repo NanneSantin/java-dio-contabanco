298089c5e55519108e8895ef0ce0b51835473be2
NanneSantin <elaine.s.santin@gmail.com>
2024-06-27 20:23:01 -0300

    feat: Project initialized with basic configuration.

---
+++ src/main/java/br/com/estudos/java/contabanco/ContabancoApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos.java.contabanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estudos/java/contabanco/ContabancoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.java.contabanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
