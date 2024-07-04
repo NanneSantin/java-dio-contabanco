f1977ddebf482671e7f92d5ce2980538a66c946b
NanneSantin <elaine.s.santin@gmail.com>
2024-07-03 22:41:15 -0300

    feat: adicionado a classe BankAccountRepository.

--- src/main/java/br/com/estudos/java/contabanco/repository/BankAccountRepository.java
+++ src/main/java/br/com/estudos/java/contabanco/repository/BankAccountRepository.java
@@ -1,2 +1,28 @@
-package br.com.estudos.java.contabanco.repository;public class BankAccountRepository {
+package br.com.estudos.java.contabanco.repository;
+
+import br.com.estudos.java.contabanco.model.BankAccount;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BankAccountRepository {
+    private Map<String, BankAccount> accountsDatabase = new HashMap<>();
+
+    public void saveAccount(BankAccount account){
+        String numAccount = account.getNumAccount() + "-" + account.getDigitCheckAccount();
+        accountsDatabase.put(numAccount, account);
+    }
+
+    public void deleteAccount(String numAccount){
+        accountsDatabase.remove(numAccount);
+        System.out.println("Conta deletada com sucesso!");
+    }
+
+    public boolean accountExists(String numAccount){
+        return accountsDatabase.containsKey(numAccount);
+    }
+
+    public BankAccount findAccount(String numAccount){
+        return accountsDatabase.get(numAccount);
+    }
 }
