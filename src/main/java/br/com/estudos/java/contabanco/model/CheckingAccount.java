310f22c7a1d673bab127a5f84bab597a760425a8
NanneSantin <elaine.s.santin@gmail.com>
2024-07-03 22:40:23 -0300

    feat: Adicionado as classes para conta poupança e conta corrente.

--- src/main/java/br/com/estudos/java/contabanco/model/CheckingAccount.java
+++ src/main/java/br/com/estudos/java/contabanco/model/CheckingAccount.java
@@ -1,2 +1,23 @@
-package br.com.estudos.java.contabanco.model;public class CheckingAccount {
+package br.com.estudos.java.contabanco.model;
+
+public class CheckingAccount extends BankAccount {
+    private static final int MONTHLY_FEE = 500; //Valor da taxa mensal em centavos.
+
+    public CheckingAccount(String customerName, String billingAddress, int initialBalance, String password) {
+        super(customerName, billingAddress, initialBalance, password);
+    }
+
+    @Override
+    public void getAccountDetails() {
+        System.out.println("Conta Corrente");
+        System.out.println("Cliente: " + getCustomerName());
+        System.out.println("Endereço: " + getBillingAddress());
+        System.out.println("Agência: " + getBankBranch());
+        System.out.println("Conta: " + getNumAccount() + "-" + getDigitCheckAccount());
+    }
+
+    @Override
+    public void applyMonthlyFee() {
+        withdraw(MONTHLY_FEE, true);
+    }
 }
--- src/main/java/br/com/estudos/java/contabanco/model/SavingAccount.java
+++ src/main/java/br/com/estudos/java/contabanco/model/SavingAccount.java
@@ -1,2 +1,24 @@
-package br.com.estudos.java.contabanco.model;public class SavingAccount {
+package br.com.estudos.java.contabanco.model;
+
+public class SavingAccount extends BankAccount {
+    private static final double INTEREST_RATE = 0.005; // Taxa de juros de 0.5%
+
+    public SavingAccount(String customerName, String billingAddress, int initialBalance, String password) {
+        super(customerName, billingAddress, initialBalance, password);
+    }
+
+    @Override
+    public void getAccountDetails() {
+        System.out.println("Conta Poupança");
+        System.out.println("Cliente: " + getCustomerName());
+        System.out.println("Endereço: " + getBillingAddress());
+        System.out.println("Agência: " + getBankBranch());
+        System.out.println("Conta: " + getNumAccount() + "-" + getDigitCheckAccount());
+    }
+
+    @Override
+    public void applyMonthlyFee() {
+        int interest = (int) (getBalance() * INTEREST_RATE);
+        deposit(interest);
+    }
 }
